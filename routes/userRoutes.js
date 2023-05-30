1aa1e967c627491ea0301f57f73582d07565f2aa
sbhole61299 <shubhambhole68@gmail.com>
2023-05-30 19:48:42 +0530

    g

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,31 @@
-hello my name is anthoinyu
+const userController = require('../controller/userController')
+const express = require('express')
+const router = express.Router()
+const { isAuthenticated } = require('../middleware/Auth')
+
+// Test Route
+router.get('/test', userController.hello)
+
+// // route to register the user
+// router.post('/register/user', userController.registeruser)
+
+
+// router to get all users
+router.get('/users', isAuthenticated, userController.getAllUsers)
+
+// router to find by id
+router.get('/userbyid', isAuthenticated, userController.findbyId)
+
+// router to delete
+router.delete('/deletebyid', isAuthenticated, userController.deletebyId)
+
+// router to update
+router.put('/updatebyid', isAuthenticated, userController.updateuser)
+
+// /router to Login
+router.post('/login', userController.login)
+
+// addroles
+router.post('/addrole', userController.addroles)
+
+module.exports = router
--- s.js
+++ s.js
@@ -1,1 +1,2 @@
 dbakslgdkahsgdkas
+abracadabra
