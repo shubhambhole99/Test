92dd2dbef703d9e1fdd560f869712d6db6a929bf
sbhole61299 <shubhambhole68@gmail.com>
2023-05-30 18:44:37 +0530

    chek:

--- s.js
+++ s.js
@@ -1,6 +1,5 @@
 
 
-<<<<<<< HEAD
 hfkjasdghfljas
 sd
 fasdhflashdfls
@@ -15,12 +14,10 @@ asdfagsdlkfa
 sfasdgf
 afjagsfas
 dfasdfjdfa
-=======
-fkjail;dhflhdfolihalisdfhjg;'iohjs
-fasdfohasl;idfh
+fkjail; dhflhdfolihalisdfhjg; 'iohjs
+fasdfohasl; idfh
 afuikasgdfkugasdf
 s
 dfasgdfkugsd[f
              sfasdghflikuagsdf
-             asdfiugsaiulfgids
->>>>>>> 81ffeba5c8c2812bab7861f4a116867bc395e2f7
+asdfiugsaiulfgids
